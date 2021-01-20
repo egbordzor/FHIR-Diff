ff3260694ccbbfeee49a7f5946d26883c54422e2
ewandavis <ewan@woodcote-consulting.com>
2021-01-20 17:19:09 +0000

    added support for -deep mode

--- FHIR-Diff.py
+++ FHIR-Diff.py
@@ -18,7 +18,7 @@ def printwithnewlines(list):
 # Directory in which JSON profiles are to be found
 
 if len(sys.argv) < 3:
-    print("Wrong number of arguments - Useage FHIR_Load.py profile1 profile2 [-all]")
+    print("Wrong number of arguments - Useage FHIR_Load.py profile1 profile2 [-all] [-deep]")
 else:
 
 # Load profile 1 in to python dictionary
@@ -37,10 +37,12 @@ else:
 # Append it to list "output1" and sort it
     output1=[]
     i=0
-
     while i < len(profile_json1["snapshot"]["element"]):
-        if profile_json1["snapshot"]["element"][i]["base"]["path"] == profile_json1["snapshot"]["element"][i]["id"]:
+        if "-deep" in sys.argv:
             output1.append(profile_json1["snapshot"]["element"][i]["id"])
+        else:
+            if profile_json1["snapshot"]["element"][i]["base"]["path"] == profile_json1["snapshot"]["element"][i]["id"]:
+                output1.append(profile_json1["snapshot"]["element"][i]["id"])
             #print(profile_json1["snapshot"]["element"][i]["id"])
         i += 1
     output1.sort()
@@ -51,20 +53,23 @@ else:
     i=0
 
     while i < len(profile_json2["snapshot"]["element"]):
-        if profile_json2["snapshot"]["element"][i]["base"]["path"] == profile_json2["snapshot"]["element"][i]["id"]:
+        if "-deep" in sys.argv:
             output2.append(profile_json2["snapshot"]["element"][i]["id"])
-            #print(profile_json2["snapshot"]["element"][i]["id"])
+        else:
+            if profile_json2["snapshot"]["element"][i]["base"]["path"] == profile_json2["snapshot"]["element"][i]["id"]:
+                output2.append(profile_json2["snapshot"]["element"][i]["id"])
+                #print(profile_json2["snapshot"]["element"][i]["id"])
         i += 1
     output2.sort()
 
-    if len(sys.argv) == 4:
-        if sys.argv[3] == "-all":
+    if "-all" in sys.argv:
+        #if sys.argv[3] == "-all":
 
-            print(profile1,"\n")
-            printwithnewlines(output1)
-            print("\n")
-            print(profile2,"\n")
-            printwithnewlines(output2)
+        print(profile1,"\n")
+        printwithnewlines(output1)
+        print("\n")
+        print(profile2,"\n")
+        printwithnewlines(output2)
 
     In_profile1_and_profile2 = [x for x in output1 if x in output2]
 
